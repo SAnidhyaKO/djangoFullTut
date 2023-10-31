fcd648bb65da4d24c04e9b0a79003ee40fc9fd68
Sanidhya koranne <sanidhyakoranne@Sanidhyas-MacBook-Air.local>
2023-10-31 13:19:15 +0530

    first_project

--- first.py
+++ first.py
@@ -1,3 +1,7 @@
+import time 
 print("hello django")
-
+time.sleep(2)
+print('lets see models in django')
+time.sleep(3)
+print("this is the third attempt")
 # i will try all sort of django here become a top g 
